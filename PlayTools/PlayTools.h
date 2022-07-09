c8b52b5b0d0c79141aa24ebc9daf9569236ab7fe
lucas lee <lixin9311@gmail.com>
2022-07-10 08:19:20 +0900

    Initial Commit

---
+++ PlayTools/PlayTools.h
@@ -0,0 +1,18 @@
+//
+//  PlayTools.h
+//  PlayTools
+//
+//  Created by lucus lee on 2022/07/10.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for PlayTools.
+FOUNDATION_EXPORT double PlayToolsVersionNumber;
+
+//! Project version string for PlayTools.
+FOUNDATION_EXPORT const unsigned char PlayToolsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PlayTools/PublicHeader.h>
+
+
