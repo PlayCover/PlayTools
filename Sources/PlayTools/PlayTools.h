ccaf497dedae0f9cb9a89877a95e3a01ae8fe26a
lucas lee <lixin9311@gmail.com>
2022-07-10 12:51:20 +0900

    seperate PlayTools from PlayCover

--- Sources/PlayTools/PlayTools.h
+++ Sources/PlayTools/PlayTools.h
@@ -2,10 +2,9 @@
 //  PlayTools.h
 //  PlayTools
 //
-//  Created by lucus lee on 2022/07/10.
-//
 
 #import <Foundation/Foundation.h>
+#import <UIKit/UIKit.h>
 
 //! Project version number for PlayTools.
 FOUNDATION_EXPORT double PlayToolsVersionNumber;
@@ -13,6 +12,8 @@ FOUNDATION_EXPORT double PlayToolsVersionNumber;
 //! Project version string for PlayTools.
 FOUNDATION_EXPORT const unsigned char PlayToolsVersionString[];
 
-// In this header, you should import all the public headers of your framework using statements like #import <PlayTools/PublicHeader.h>
-
-
+#import "PTFakeMetaTouch.h"
+#import "IOHIDEvent+KIF.h"
+#import "UIApplication+Private.h"
+#import "UIEvent+Private.h"
+#import "UITouch+Private.h"
