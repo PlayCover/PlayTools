5048287c508e0afe01ff316c06aff0f2539020ca
JoseMoreville <18040006@utculiacan.edu.mx>
2022-07-24 12:11:15 -0600

    fix

--- PlayTools/PlayScreen.swift
+++ PlayTools/PlayScreen.swift
@@ -39,7 +39,7 @@ extension CGRect {
     }
     
     func toAspectRatioReversed() -> CGRect {
-        return CGRect(x: minX, y : minY, width: width, height: width / UIScreen.aspectRatio)
+        return CGRect(x: minX, y : minY, width: width, height: width )
     }
    
 }
