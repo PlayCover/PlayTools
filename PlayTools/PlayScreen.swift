18de4b1d180bf9e2d98afdf207e4df4ff4f0568f
JoseMoreville <18040006@utculiacan.edu.mx>
2022-07-24 12:07:21 -0600

    modify full view

--- PlayTools/PlayScreen.swift
+++ PlayTools/PlayScreen.swift
@@ -20,7 +20,7 @@ extension CGSize {
     }
     
     func toAspectRatio() -> CGSize {
-        return CGSize(width: width, height: height)
+        return CGSize(width: 1920, height: 1080)
     }
 }
 
@@ -35,11 +35,11 @@ extension CGRect {
     }
     
     func toAspectRatio() -> CGRect {
-        return CGRect(x: minX, y : minY, width:width, height: height)
+        return CGRect(x: minX, y : minY, width: 1920 , height: height)
     }
     
     func toAspectRatioReversed() -> CGRect {
-        return CGRect(x: minX, y : minY, width: width, height: height)
+        return CGRect(x: minX, y : minY, width: width, height: width / UIScreen.aspectRatio)
     }
    
 }
