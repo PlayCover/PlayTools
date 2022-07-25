49f8e4bcabc270b70dc090e03aa2f59283f99c75
JoseMoreville <18040006@utculiacan.edu.mx>
2022-07-24 19:54:15 -0600

    1920

--- PlayTools/PlayScreen.swift
+++ PlayTools/PlayScreen.swift
@@ -9,7 +9,7 @@ import SwiftUI
 import AVFoundation
 
 let screen = PlayScreen.shared
-let mainScreenWidth: CGFloat =  PlaySettings.shared.windowSizeWidth == 1920 ? 1920 : 1920
+let mainScreenWidth: CGFloat =  1920
 let mainScreenHeight: CGFloat = PlaySettings.shared.windowSizeHeight
 
 extension CGSize {
