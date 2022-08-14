50de6f60e2c991452ad188c97cf36c1faad89ffa
Xyct <87l46110@gmail.com>
2022-08-14 14:54:31 +0800

    remove macros from menu

--- PlayTools/Controls/MenuController.swift
+++ PlayTools/Controls/MenuController.swift
@@ -70,15 +70,15 @@ struct CommandsList {
 var keymapping = ["Open/Close Keymapping Editor", "Delete selected element", "Upsize selected element", "Downsize selected element"]
 var keymappingSelectors = [#selector(UIViewController.switchEditorMode(_:)), #selector(UIViewController.removeElement(_:)), #selector(UIViewController.upscaleElement(_:)), #selector(UIViewController.downscaleElement(_:))]
 
-var macros = ["Start/Stop touch recording", "Start/Stop replaying", "Loop/Stop replaying"]
-var macroSelectors = [#selector(UIViewController.switchTouchRecording(_:)), #selector(UIViewController.switchTouchReplaying(_:)), #selector(UIViewController.switchTouchReplayingLoop(_:))]
+//var macros = ["Start/Stop touch recording", "Start/Stop replaying", "Loop/Stop replaying"]
+//var macroSelectors = [#selector(UIViewController.switchTouchRecording(_:)), #selector(UIViewController.switchTouchReplaying(_:)), #selector(UIViewController.switchTouchReplayingLoop(_:))]
 
 class MenuController {
     
     init(with builder: UIMenuBuilder) {
         if #available(iOS 15.0, *) {
             builder.insertSibling(MenuController.keymappingMenu(), afterMenu: .view)
-            builder.insertSibling(MenuController.macrosMenu(), afterMenu: .keymappingMenu)
+//            builder.insertSibling(MenuController.macrosMenu(), afterMenu: .keymappingMenu)
         } else {
             
         }
@@ -111,32 +111,32 @@ class MenuController {
                       children: [arrowKeysGroup])
     }
     
-    @available(iOS 15.0, *)
-    class func macrosMenu() -> UIMenu {
-        let keyCommands = ["O","P","L" ]
-        
-        let macroCommands = zip(keyCommands, macros).map { (command, btn) in
-            UIKeyCommand(title: btn,
-                         image: nil,
-                         action: macroSelectors[macros.firstIndex(of: btn)!],
-                         input: command,
-                         modifierFlags: .command,
-                         propertyList: [CommandsList.MacrosToolbox: btn]
-            )
-        }
-        
-        let macrosKeysGroup = UIMenu(title: "",
-                                    image: nil,
-                                         identifier: .macrosMenu,
-                                    options: .displayInline,
-                                    children: macroCommands)
-        
-        return UIMenu(title: NSLocalizedString("Macros", comment: ""),
-                      image: nil,
-                      identifier: .macrosActionsMenu,
-                      options: [],
-                      children: [macrosKeysGroup])
-    }
+//    @available(iOS 15.0, *)
+//    class func macrosMenu() -> UIMenu {
+//        let keyCommands = ["O","P","L" ]
+//
+//        let macroCommands = zip(keyCommands, macros).map { (command, btn) in
+//            UIKeyCommand(title: btn,
+//                         image: nil,
+//                         action: macroSelectors[macros.firstIndex(of: btn)!],
+//                         input: command,
+//                         modifierFlags: .command,
+//                         propertyList: [CommandsList.MacrosToolbox: btn]
+//            )
+//        }
+//
+//        let macrosKeysGroup = UIMenu(title: "",
+//                                    image: nil,
+//                                         identifier: .macrosMenu,
+//                                    options: .displayInline,
+//                                    children: macroCommands)
+//
+//        return UIMenu(title: NSLocalizedString("Macros", comment: ""),
+//                      image: nil,
+//                      identifier: .macrosActionsMenu,
+//                      options: [],
+//                      children: [macrosKeysGroup])
+//    }
     
     
 }
@@ -145,6 +145,6 @@ class MenuController {
 extension UIMenu.Identifier {
     static var keymappingMenu: UIMenu.Identifier { UIMenu.Identifier("io.playcover.PlayTools.menus.editor") }
     static var keymappingOptionsMenu: UIMenu.Identifier { UIMenu.Identifier("io.playcover.PlayTools.menus.keymapping") }
-    static var macrosMenu: UIMenu.Identifier { UIMenu.Identifier("io.playcover.PlayTools.menus.macros") }
-    static var macrosActionsMenu: UIMenu.Identifier { UIMenu.Identifier("io.playcover.PlayTools.menus.touchrecord") }
+//    static var macrosMenu: UIMenu.Identifier { UIMenu.Identifier("io.playcover.PlayTools.menus.macros") }
+//    static var macrosActionsMenu: UIMenu.Identifier { UIMenu.Identifier("io.playcover.PlayTools.menus.touchrecord") }
 }
