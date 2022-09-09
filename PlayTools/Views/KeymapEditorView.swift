8f49c117f8e2374d22a98df7a413fef774cf0d14
Isaac Marovitz <isaacryu@icloud.com>
2022-09-09 19:00:58 -0400

    Add other button models

--- PlayTools/Views/KeymapEditorView.swift
+++ PlayTools/Views/KeymapEditorView.swift
@@ -14,8 +14,31 @@ struct KeymapEditorView: View {
                 ForEach(Keymapping.shared.keymapData.buttonModels, id: \.transform, content: { data in
                     ButtonView(xCoord: data.transform.xCoord * screen.width,
                                yCoord: data.transform.yCoord * screen.height,
-                               key: KeyCodeNames.keyCodes[data.keyCode]!,
-                               size: data.transform.size*10)
+                               size: data.transform.size * 10,
+                               key: data.keyCode.keyCodeString())
+                })
+                ForEach(Keymapping.shared.keymapData.draggableButtonModels, id: \.transform, content: { data in
+                    ButtonView(xCoord: data.transform.xCoord * screen.width,
+                               yCoord: data.transform.yCoord * screen.height,
+                               size: data.transform.size * 10,
+                               key: data.keyCode.keyCodeString())
+                    
+                })
+                ForEach(Keymapping.shared.keymapData.joystickModel, id: \.transform, content: { data in
+                    JoystickView(xCoord: data.transform.xCoord * screen.width,
+                                 yCoord: data.transform.yCoord * screen.height,
+                                 size: data.transform.size * 10,
+                                 upKey: data.upKeyCode.keyCodeString(),
+                                 rightKey: data.rightKeyCode.keyCodeString(),
+                                 downKey: data.downKeyCode.keyCodeString(),
+                                 leftKey: data.leftKeyCode.keyCodeString())
+                    
+                })
+                ForEach(Keymapping.shared.keymapData.mouseAreaModel, id: \.transform, content: { data in
+                    MouseArea(xCoord: data.transform.xCoord * screen.width,
+                               yCoord: data.transform.yCoord * screen.height,
+                               size: data.transform.size * 10)
+                    
                 })
             }
         }
@@ -24,9 +47,14 @@ struct KeymapEditorView: View {
         .background(Color.black.opacity(0.3))
         .contextMenu {
             SwiftUI.Button(action: {
-                print("Single Key")
+                print("Button")
             }, label: {
-                Text("Single Key")
+                Text("Button")
+            })
+            SwiftUI.Button(action: {
+                print("Dragable Button")
+            }, label: {
+                Text("Dragable Button")
             })
             SwiftUI.Button(action: {
                 print("Joystick")
@@ -45,8 +73,9 @@ struct KeymapEditorView: View {
 struct ButtonView: View {
     @State var xCoord: CGFloat
     @State var yCoord: CGFloat
-    @State var key: String
     @State var size: CGFloat
+
+    @State var key: String
 
     var body: some View {
         ZStack {
@@ -63,5 +92,71 @@ struct ButtonView: View {
                     yCoord = gesture.location.y
                 }
         )
+    }
+}
+
+struct JoystickView: View {
+    @State var xCoord: CGFloat
+    @State var yCoord: CGFloat
+    @State var size: CGFloat
+
+    @State var upKey: String
+    @State var rightKey: String
+    @State var downKey: String
+    @State var leftKey: String
+
+    var body: some View {
+        ZStack {
+            Circle()
+                .fill(.thinMaterial)
+                .frame(width: size, height: size)
+            HStack {
+                VStack {
+                    ButtonView(xCoord: xCoord, yCoord: yCoord, size: size, key: upKey)
+                    ButtonView(xCoord: xCoord, yCoord: yCoord, size: size, key: rightKey)
+                }
+                VStack {
+                    ButtonView(xCoord: xCoord, yCoord: yCoord, size: size, key: leftKey)
+                    ButtonView(xCoord: xCoord, yCoord: yCoord, size: size, key: downKey)
+                }
+            }
+            .rotationEffect(Angle(degrees: 45))
+        }
+        .position(x: xCoord, y: yCoord)
+        .gesture(
+            DragGesture()
+                .onChanged { gesture in
+                    xCoord = gesture.location.x
+                    yCoord = gesture.location.y
+                }
+        )
+    }
+}
+
+struct MouseAreaView: View {
+    @State var xCoord: CGFloat
+    @State var yCoord: CGFloat
+    @State var size: CGFloat
+
+    var body: some View {
+        ZStack {
+            Circle()
+                .fill(.thinMaterial)
+                .frame(width: size, height: size)
+        }
+        .position(x: xCoord, y: yCoord)
+        .gesture(
+            DragGesture()
+                .onChanged { gesture in
+                    xCoord = gesture.location.x
+                    yCoord = gesture.location.y
+                }
+        )
+    }
+}
+
+extension Int {
+    func keyCodeString() -> String {
+        return KeyCodeNames.keyCodes[self]!
     }
 }
