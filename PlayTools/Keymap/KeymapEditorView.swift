f441fffa5216076a49fb44225df312b627649201
Isaac Marovitz <isaacryu@icloud.com>
2022-11-01 17:04:08 -0400

    Selection

--- PlayTools/Keymap/KeymapEditorView.swift
+++ PlayTools/Keymap/KeymapEditorView.swift
@@ -8,6 +8,8 @@
 import SwiftUI
 
 struct KeymapEditorView: View {
+    @State var selected: KeyModelTransform?
+
     var body: some View {
         ZStack {
             Color.black.opacity(0.3)
@@ -36,41 +38,34 @@ struct KeymapEditorView: View {
                     })
                 }
             ForEach(Keymapping.shared.keymapData.buttonModels, id: \.transform) { data in
-                ButtonView(xCoord: data.transform.xCoord * screen.width,
-                           yCoord: data.transform.yCoord * screen.height,
-                           size: data.transform.size * 10,
+                ButtonView(transform: data.transform,
+                           selected: $selected,
                            key: data.keyCode.keyCodeString())
             }
-            /*ForEach(Keymapping.shared.keymapData.draggableButtonModels, id: \.transform, content: { data in
-                ButtonView(xCoord: data.transform.xCoord * screen.width,
-                           yCoord: data.transform.yCoord * screen.height,
-                           size: data.transform.size * 10,
-                           key: data.keyCode.keyCodeString())
-            })*/
             ForEach(Keymapping.shared.keymapData.joystickModel, id: \.transform, content: { data in
-                JoystickView(xCoord: data.transform.xCoord * screen.width,
-                             yCoord: data.transform.yCoord * screen.height,
-                             size: data.transform.size * 10,
+                JoystickView(transform: data.transform,
+                             selected: $selected,
                              upKey: data.upKeyCode.keyCodeString(),
                              rightKey: data.rightKeyCode.keyCodeString(),
                              downKey: data.downKeyCode.keyCodeString(),
                              leftKey: data.leftKeyCode.keyCodeString())
             })
             ForEach(Keymapping.shared.keymapData.mouseAreaModel, id: \.transform, content: { data in
-                MouseAreaView(xCoord: data.transform.xCoord * screen.width,
-                              yCoord: data.transform.yCoord * screen.height,
-                              size: data.transform.size * 10)
+                MouseAreaView(transform: data.transform,
+                              selected: $selected)
             })
         }
         .ignoresSafeArea(.all)
         .frame(maxWidth: .infinity, maxHeight: .infinity)
+        .onTapGesture {
+            selected = nil
+        }
     }
 }
 
 struct ButtonView: View {
-    @State var xCoord: CGFloat
-    @State var yCoord: CGFloat
-    @State var size: CGFloat
+    @State var transform: KeyModelTransform
+    @Binding var selected: KeyModelTransform?
 
     @State var key: String
 
@@ -78,23 +73,26 @@ struct ButtonView: View {
         Text(key)
             .frame(width: 80, height: 80)
             .background(Circle()
-                .stroke(.white, lineWidth: 1)
+                .stroke(.white, lineWidth: selected == transform ? 1 : 0)
                 .background(Circle().fill(.regularMaterial)))
-            .position(x: xCoord, y: yCoord)
+            .position(x: transform.xCoord * screen.width,
+                      y: transform.yCoord * screen.height)
+            .onTapGesture {
+                selected = transform
+            }
             .gesture(
                 DragGesture()
                     .onChanged { gesture in
-                        xCoord = gesture.location.x
-                        yCoord = gesture.location.y
+                        transform.xCoord = gesture.location.x
+                        transform.yCoord = gesture.location.y
                     }
             )
     }
 }
 
 struct JoystickView: View {
-    @State var xCoord: CGFloat
-    @State var yCoord: CGFloat
-    @State var size: CGFloat
+    @State var transform: KeyModelTransform
+    @Binding var selected: KeyModelTransform?
 
     @State var upKey: String
     @State var rightKey: String
@@ -104,7 +102,7 @@ struct JoystickView: View {
     var body: some View {
         ZStack {
             Circle()
-                .stroke(.white, lineWidth: 1)
+                .stroke(.white, lineWidth: selected == transform ? 1 : 0)
                 .frame(width: 200, height: 200)
                 .background(Circle().fill(.regularMaterial))
             VStack(alignment: .center, spacing: 15) {
@@ -131,34 +129,41 @@ struct JoystickView: View {
             }
             .rotationEffect(Angle(degrees: 45))
         }
-        .position(x: xCoord, y: yCoord)
+        .position(x: transform.xCoord * screen.width,
+                  y: transform.yCoord * screen.height)
+        .onTapGesture {
+            selected = transform
+        }
         .gesture(
             DragGesture()
                 .onChanged { gesture in
-                    xCoord = gesture.location.x
-                    yCoord = gesture.location.y
+                    transform.xCoord = gesture.location.x
+                    transform.yCoord = gesture.location.y
                 }
         )
     }
 }
 
 struct MouseAreaView: View {
-    @State var xCoord: CGFloat
-    @State var yCoord: CGFloat
-    @State var size: CGFloat
+    @State var transform: KeyModelTransform
+    @Binding var selected: KeyModelTransform?
 
     var body: some View {
         Text("")
             .frame(width: 200, height: 200)
             .background(Circle()
-                .stroke(.white, lineWidth: 1)
+                .stroke(.white, lineWidth: selected == transform ? 1 : 0)
                 .background(Circle().fill(.regularMaterial)))
-            .position(x: xCoord, y: yCoord)
+            .position(x: transform.xCoord * screen.width,
+                      y: transform.yCoord * screen.height)
+            .onTapGesture {
+                selected = transform
+            }
             .gesture(
                 DragGesture()
                     .onChanged { gesture in
-                        xCoord = gesture.location.x
-                        yCoord = gesture.location.y
+                        transform.xCoord = gesture.location.x
+                        transform.yCoord = gesture.location.y
                     }
             )
     }
