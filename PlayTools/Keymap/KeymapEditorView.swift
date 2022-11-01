492f4c3c1a8508aee0adc093b619861500831478
Isaac Marovitz <isaacryu@icloud.com>
2022-11-01 17:59:00 -0400

    Scale

--- PlayTools/Keymap/KeymapEditorView.swift
+++ PlayTools/Keymap/KeymapEditorView.swift
@@ -76,7 +76,7 @@ struct ButtonView: View {
                 .stroke(selected != transform ? .white : .accentColor,
                         lineWidth: selected != transform ? 0 : 5)
                 .background(Circle().fill(.regularMaterial)))
-            .scaleEffect(transform.size / 10)
+            .scaleEffect(1 + transform.size / 10)
             .position(x: transform.xCoord * screen.width,
                       y: transform.yCoord * screen.height)
             .onTapGesture {
@@ -87,6 +87,12 @@ struct ButtonView: View {
                     .onChanged { gesture in
                         transform.xCoord = gesture.location.x
                         transform.yCoord = gesture.location.y
+                    }
+            )
+            .gesture(
+                MagnificationGesture()
+                    .onChanged { value in
+                        transform.size = (value - 1) * 10
                     }
             )
             .zIndex(selected != transform ? 0 : 10)
