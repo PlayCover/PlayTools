976f0adcec6353386384a1af08ab4dae15460c45
José Moreno <47700212+JoseMoreville@users.noreply.github.com>
2023-01-30 13:29:24 -0700

    Fix for default screen

--- PlayTools/Controls/PTFakeTouch/NSObject+Swizzle.m
+++ PlayTools/Controls/PTFakeTouch/NSObject+Swizzle.m
@@ -82,6 +82,16 @@ __attribute__((visibility("hidden")))
     return 2.0;
 }
 
+- (double) get_default_height {
+    return [[UIScreen mainScreen] bounds].size.height;
+    
+}
+- (double) get_default_width {
+    return [[UIScreen mainScreen] bounds].size.width;
+    
+}
+
+
 
 bool menuWasCreated = false;
 - (id) initWithRootMenuHook:(id)rootMenu {
@@ -128,6 +138,22 @@ bool menuWasCreated = false;
         [objc_getClass("FBSDisplayMode") swizzleInstanceMethod:@selector(size) withMethod:@selector(hook_size)];
         
         // This actually fixes Apple mess at MacOS 13.2
+        [objc_getClass("UIDevice") swizzleInstanceMethod:@selector(orientation) withMethod:@selector(hook_orientation)];
+        [objc_getClass("UIScreen") swizzleInstanceMethod:@selector(nativeBounds) withMethod:@selector(hook_nativeBounds)];
+        [objc_getClass("UIScreen") swizzleInstanceMethod:@selector(nativeScale) withMethod:@selector(hook_nativeScale)];
+        [objc_getClass("UIScreen") swizzleInstanceMethod:@selector(scale) withMethod:@selector(hook_scale)];
+    } else {
+        // This fixes default frame/scene issues post MacOS 13.2
+        CGFloat newValueW = (CGFloat) [self get_default_width];
+        [[PlaySettings shared] setValue:@(newValueW) forKey:@"windowSizeWidth"];
+        
+        CGFloat newValueH = (CGFloat)[self get_default_height];
+        [[PlaySettings shared] setValue:@(newValueH) forKey:@"windowSizeHeight"];
+
+        [objc_getClass("FBSSceneSettings") swizzleInstanceMethod:@selector(frame) withMethod:@selector(hook_frame)];
+        [objc_getClass("FBSSceneSettings") swizzleInstanceMethod:@selector(bounds) withMethod:@selector(hook_bounds)];
+        [objc_getClass("FBSDisplayMode") swizzleInstanceMethod:@selector(size) withMethod:@selector(hook_size)];
+        
         [objc_getClass("UIDevice") swizzleInstanceMethod:@selector(orientation) withMethod:@selector(hook_orientation)];
         [objc_getClass("UIScreen") swizzleInstanceMethod:@selector(nativeBounds) withMethod:@selector(hook_nativeBounds)];
         [objc_getClass("UIScreen") swizzleInstanceMethod:@selector(nativeScale) withMethod:@selector(hook_nativeScale)];
