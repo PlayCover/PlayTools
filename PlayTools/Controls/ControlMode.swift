25714cc0cbb2d8abc68c1bb35067e79a9924c58c
Xyct <87l46110@gmail.com>
2023-04-28 11:31:31 +0800

    support fixed and free joystick

--- PlayTools/Controls/ControlMode.swift
+++ PlayTools/Controls/ControlMode.swift
@@ -11,6 +11,16 @@ public class ControlMode {
 
     static public let mode = ControlMode()
     public var visible: Bool = true
+    public var keyboardMapped = true
+
+    public static func trySwap() -> Bool {
+        if PlayInput.shouldLockCursor {
+            mode.show(!mode.visible)
+            return true
+        }
+        mode.show(true)
+        return false
+    }
 
     func show(_ show: Bool) {
         if !editor.editorMode {
@@ -28,8 +38,7 @@ public class ControlMode {
                     if screen.fullscreen {
                         screen.switchDock(false)
                     }
-
-                    PlayInput.shared.setup()
+//                    PlayInput.shared.setup()
                 }
             }
             Toucher.writeLog(logMessage: "cursor show switched to \(show)")
