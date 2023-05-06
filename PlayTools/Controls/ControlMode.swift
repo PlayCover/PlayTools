2636736b00c88b13aaa450cd7c658339e3ac5e51
Xyct <87l46110@gmail.com>
2023-05-07 05:21:53 +0800

    invalidate when disable keyboard mapping

--- PlayTools/Controls/ControlMode.swift
+++ PlayTools/Controls/ControlMode.swift
@@ -22,27 +22,46 @@ public class ControlMode {
         return false
     }
 
+    func setMapping(_ mapped: Bool) {
+        keyboardMapped = mapped
+        if mapped {
+            PlayInput.shared.parseKeymap()
+        } else {
+            PlayInput.shared.invalidate()
+        }
+    }
+
     func show(_ show: Bool) {
         if !editor.editorMode {
             if show {
                 if !visible {
+                    NotificationCenter.default.post(name: NSNotification.Name.playtoolsCursorWillShow,
+                                                    object: nil, userInfo: [:])
                     if screen.fullscreen {
                         screen.switchDock(true)
                     }
                     AKInterface.shared!.unhideCursor()
-//                    PlayInput.shared.invalidate()
                 }
             } else {
                 if visible {
+                    NotificationCenter.default.post(name: NSNotification.Name.playtoolsCursorWillHide,
+                                                    object: nil, userInfo: [:])
                     AKInterface.shared!.hideCursor()
                     if screen.fullscreen {
                         screen.switchDock(false)
                     }
-//                    PlayInput.shared.setup()
                 }
             }
             Toucher.writeLog(logMessage: "cursor show switched to \(show)")
             visible = show
         }
     }
+}
+
+extension NSNotification.Name {
+    public static let playtoolsCursorWillHide: NSNotification.Name
+                    = NSNotification.Name("playtools.cursorWillHide")
+
+    public static let playtoolsCursorWillShow: NSNotification.Name
+                    = NSNotification.Name("playtools.cursorWillShow")
 }
