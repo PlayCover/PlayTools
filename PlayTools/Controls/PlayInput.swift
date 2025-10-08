413d938db5e182e9f8a63e027ab5dbbfaa8cfadf
viatearz <viatearz8056@outlook.com>
2025-10-09 00:59:12 +0800

    fix: disable built-in mouse support to avoid click conflicts (#201)

--- PlayTools/Controls/PlayInput.swift
+++ PlayTools/Controls/PlayInput.swift
@@ -1,5 +1,6 @@
 import Foundation
 import UIKit
+import GameController
 
 // This class is a coordinator (and module entrance), coordinating other concrete classes
 
@@ -18,6 +19,10 @@ class PlayInput {
         // drain the dispatch queue every frame for responding to GCController events
         let displaylink = CADisplayLink(target: self, selector: #selector(drainMainDispatchQueue))
         displaylink.add(to: .main, forMode: .common)
+
+        if PlaySettings.shared.disableBuiltinMouse {
+            simulateGCMouseDisconnect()
+        }
 
         if !PlaySettings.shared.keymapping {
             return
@@ -40,5 +45,32 @@ class PlayInput {
             Toast.initialize()
         }
         mode.initialize()
+    }
+
+    private func simulateGCMouseDisconnect() {
+        NotificationCenter.default.addObserver(
+            forName: .GCMouseDidConnect,
+            object: nil,
+            queue: .main
+        ) { nofitication in
+            guard let mouse = nofitication.object as? GCMouse else {
+                return
+            }
+            DispatchQueue.main.asyncAfter(deadline: .now() + .milliseconds(1)) {
+                NotificationCenter.default.post(name: .GCMouseDidDisconnect, object: mouse)
+                mouse.mouseInput?.leftButton.pressedChangedHandler = nil
+                mouse.mouseInput?.leftButton.valueChangedHandler = nil
+                mouse.mouseInput?.rightButton?.pressedChangedHandler = nil
+                mouse.mouseInput?.rightButton?.valueChangedHandler = nil
+                mouse.mouseInput?.middleButton?.pressedChangedHandler = nil
+                mouse.mouseInput?.middleButton?.valueChangedHandler = nil
+                mouse.mouseInput?.auxiliaryButtons?.forEach { button in
+                    button.pressedChangedHandler = nil
+                    button.valueChangedHandler = nil
+                }
+                mouse.mouseInput?.scroll.valueChangedHandler = nil
+                mouse.mouseInput?.mouseMovedHandler = nil
+            }
+        }
     }
 }
