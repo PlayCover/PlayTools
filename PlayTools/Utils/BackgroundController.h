d5a10c56702b433c30b6aa7694ab4651ed939db7
Sinon <34333654+Catta1997@users.noreply.github.com>
2026-02-21 11:55:51 +0100

    feat: Better Controller Fix on Tahoe

---
+++ PlayTools/Utils/BackgroundController.h
@@ -0,0 +1,18 @@
+//
+//  BatteryLevel.h
+//  PlayTools
+//
+//  Created by Edoardo C. on 21/02/26.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface NSObject (SwizzleBackgroundController)
+
+- (void)swizzleInstanceMethod:(SEL)origSelector withMethod:(SEL)newSelector;
+
+@end
+
+NS_ASSUME_NONNULL_END
